f3aa0022b567176efff290a29267245255accc69
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-03-08 16:45:10 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseBasic::Application.config.secret_token = '3ffc0012cf1f838fb3af8893abbb5652ed19d6208ab42905c4b1bf37a4db48f0816878277a42d831942486fc54390888782550dbfc0b44a08726d58abb35ecf6'
