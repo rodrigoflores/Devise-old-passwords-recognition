3cd0c7953f73a88a6b465927e6c3cf156062aaa8
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-03-08 17:10:08 -0300

    Added a test to sign in

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -13,4 +13,19 @@ feature 'Basic sign up and login' do
 
     page.should have_content("Welcome! You have signed up successfully.")
   end
+
+  scenario "does the sign in" do
+    email = "rodrigo.flores@plataformatec.com.br"
+    password = "idontusethispassword"
+    user = User.create!(:email => email, :password => password)
+
+    visit new_user_session_path
+
+    fill_in "Email", :with => email
+    fill_in "Password", :with => password
+
+    click_button "Sign in"
+
+    page.should have_content("Signed in successfully.")
+  end
 end
