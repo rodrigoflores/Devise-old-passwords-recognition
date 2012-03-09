0fa441bb17cdaa387f6c67432b2d42ad11859f0f
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-03-09 15:00:03 -0300

    Added a test for the callback

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,12 @@ describe User do
       user.old_password?("oldpassword").should be_true
     end
   end
+
+  describe "callbacks" do
+    it "adds the encrypted password on passwords table" do
+      expect {
+        User.create!(:email => "a@b.com", :password => "oldpassword")
+      }.to change(Password, :count).by(1)
+    end
+  end
 end
